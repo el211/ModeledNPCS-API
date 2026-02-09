d6f3ea3661ab5bd6e19476c81acad98354b7ee2e
el211 <saireelias@gmail.com>
2026-02-09 19:56:38 +0100

    Add missing NPCInteractEvent class

--- src/main/java/fr/elias/npcs/events/NPCInteractEvent.java
+++ src/main/java/fr/elias/npcs/events/NPCInteractEvent.java
@@ -1,5 +1,6 @@
 package fr.elias.npcs.events;
 
+import fr.elias.npcs.data.INPCData;  // ✅ FIXED: Changed from api to data
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
@@ -7,28 +8,59 @@ import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Called when a player interacts with an NPC
+ * Event fired when a player interacts with an NPC.
+ * This event is part of the ModeledNPCS API.
  */
 public class NPCInteractEvent extends Event implements Cancellable {
 
     private static final HandlerList HANDLERS = new HandlerList();
-    private boolean cancelled = false;
 
     private final Player player;
-    private final int npcId;
+    private final INPCData npcData;
+    private final InteractionType interactionType;
+    private boolean cancelled = false;
 
-    public NPCInteractEvent(@NotNull Player player, int npcId) {
+    /**
+     * Creates a new NPCInteractEvent.
+     *
+     * @param player The player who interacted with the NPC
+     * @param npcData The NPC data
+     * @param interactionType The type of interaction
+     */
+    public NPCInteractEvent(@NotNull Player player, @NotNull INPCData npcData, @NotNull InteractionType interactionType) {
         this.player = player;
-        this.npcId = npcId;
+        this.npcData = npcData;
+        this.interactionType = interactionType;
     }
 
+    /**
+     * Gets the player who interacted with the NPC.
+     *
+     * @return The player
+     */
     @NotNull
     public Player getPlayer() {
         return player;
     }
 
-    public int getNpcId() {
-        return npcId;
+    /**
+     * Gets the NPC data.
+     *
+     * @return The NPC data
+     */
+    @NotNull
+    public INPCData getNPCData() {
+        return npcData;
+    }
+
+    /**
+     * Gets the type of interaction.
+     *
+     * @return The interaction type
+     */
+    @NotNull
+    public InteractionType getInteractionType() {
+        return interactionType;
     }
 
     @Override
@@ -50,5 +82,30 @@ public class NPCInteractEvent extends Event implements Cancellable {
     @NotNull
     public static HandlerList getHandlerList() {
         return HANDLERS;
+    }
+
+    /**
+     * The type of interaction with the NPC.
+     */
+    public enum InteractionType {
+        /**
+         * Left click interaction
+         */
+        LEFT_CLICK,
+
+        /**
+         * Right click interaction
+         */
+        RIGHT_CLICK,
+
+        /**
+         * Shift + left click interaction
+         */
+        SHIFT_LEFT_CLICK,
+
+        /**
+         * Shift + right click interaction
+         */
+        SHIFT_RIGHT_CLICK
     }
 }
